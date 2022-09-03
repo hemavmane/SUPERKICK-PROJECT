eef78361077e842bb9f8206ab3b2c844d2b95dbe
Admin <hemamane5376@gmail.com>
2022-09-04 01:21:26 +0530

    ass

--- Slider.js
+++ Slider.js
@@ -31,33 +31,11 @@ function show_image(initial) {
 
 setInterval(show_image, 3000);
 
-let dotbtn = document.querySelector(".dots");
-let container = document.getElementById("image");
 
-let arr = [
-  ["./img3/9.jpg", "dot1"],
-  ["./img3/8.jpg", "dot2"],
-  ["./img3/7.jpg", "dot3"],
-  ["./img3/6.jpg", "dot4"],
-  ["./img3/5.jpg", "dot5"],
-  ["./img3/4.jpg", "dot6"],
-];
 
-function slide(e) {
-  for (let i = 0; i < arr.length; i++) {
-    container.innerHTML = "";
-    if (
-      e.target.innerText == "dot1" ||
-      e.target.innerText == "dot2" ||
-      e.target.innerText == "dot3" ||
-      e.target.innerText == "dot4" ||
-      e.target.innerText == "dot5" ||
-      e.target.innerText == "dot6"
-    )
-      container.innerHTML = `<div id="image">
-    <span><img src="${arr[i][0]}" alt="" /></span>
-    </div>`;
-  }
-}
 
-dotbtn.addEventListener("click", slide);
+
+
+
+
+
--- product.js
+++ product.js
@@ -1,67 +1,110 @@
 let arr = [
-  { img: "./img1/alpargata rs 3.jpg",Brand:"Toms",name:"ALPARGATA RECYCLED COTTON CANVAS ‘MAASTRICHTNAVY’",price:"₹3,999" },
-  { img: "./img1/canvas natural rs 4,999.jpg",Brand:"Toms",name:"ALPARGATA HERITAGE CANVAS ‘NATURAL / PEACH’",price:"₹4,999" },
-  { img: "./img1/cotton canvas rose red rs 3,999.jpg",Brand:"Toms",name:"ALPARGATA RECYCLED COTTON CANVAS 'ROSE RED'",price: "₹3,999" },
-  { img: "./img1/espandrille alparag navy mariya rs 5,499.jpg", Brand:"Toms",name:"ESPADRILLE ALPARGATA ‘NAVY MARINA’",price: "₹5,499" },
-  { img: "./img1/espandrille bkack rs 4.jpg",Brand:"Toms",name:"ESPADRILLE ALPARGATA ‘BLACK SLUBBY’",price:"₹4,999" },
-  { img: "./img1/forum.jpg",Brand:"Adidas",name:"FORUM LUXE LOW ‘CWHITE / CRAOCH’",price:"₹11,999" },
-  { img: "./img1/nitebal.jpg",Brand:"Adidas Original",name:"NITEBALL ‘CORE BLACK / CLOUD WHITE’",price:"₹11,999" },
-  { img: "./img1/jordan.jpg",Brand:"Nike",name:"JORDAN 5 ‘DARK CONCORD’",price:"₹18,395" },
-  { img: "./img1/air.jpg",Brand:"Nike",name:"AIR JORDAN 14 ‘LIGHT GINGER’",price:"₹18,395" },
+  {
+    img: "./img1/alpargata rs 3.jpg",
+    Brand: "Toms",
+    name: "ALPARGATA RECYCLED COTTON CANVAS ‘MAASTRICHTNAVY’",
+    price: "₹3,999",
+  },
+  {
+    img: "./img1/canvas natural rs 4,999.jpg",
+    Brand: "Toms",
+    name: "ALPARGATA HERITAGE CANVAS ‘NATURAL / PEACH’",
+    price: "₹4,999",
+  },
+  {
+    img: "./img1/cotton canvas rose red rs 3,999.jpg",
+    Brand: "Toms",
+    name: "ALPARGATA RECYCLED COTTON CANVAS 'ROSE RED'",
+    price: "₹3,999",
+  },
+  {
+    img: "./img1/espandrille alparag navy mariya rs 5,499.jpg",
+    Brand: "Toms",
+    name: "ESPADRILLE ALPARGATA ‘NAVY MARINA’",
+    price: "₹5,499",
+  },
+  {
+    img: "./img1/espandrille bkack rs 4.jpg",
+    Brand: "Toms",
+    name: "ESPADRILLE ALPARGATA ‘BLACK SLUBBY’",
+    price: "₹4,999",
+  },
+  {
+    img: "./img1/forum.jpg",
+    Brand: "Adidas",
+    name: "FORUM LUXE LOW ‘CWHITE / CRAOCH’",
+    price: "₹11,999",
+  },
+  {
+    img: "./img1/nitebal.jpg",
+    Brand: "Adidas Original",
+    name: "NITEBALL ‘CORE BLACK / CLOUD WHITE’",
+    price: "₹11,999",
+  },
+  {
+    img: "./img1/jordan.jpg",
+    Brand: "Nike",
+    name: "JORDAN 5 ‘DARK CONCORD’",
+    price: "₹18,395",
+  },
+  {
+    img: "./img1/air.jpg",
+    Brand: "Nike",
+    name: "AIR JORDAN 14 ‘LIGHT GINGER’",
+    price: "₹18,395",
+  },
 ];
 
+let container = document.querySelector(".images_container");
+let prebtn = document.querySelector(".btn1");
+let nextbtn = document.querySelector(".btn2");
 
-let container=document.querySelector(".images_container")
-let prebtn=document.querySelector(".btn1")
-let nextbtn=document.querySelector(".btn2")
+let prev = document.createElement("button");
+let flag = 0;
+function previous() {
+  container.innerHTML = "";
+  if (flag == 4) {
+    flag = 1;
+  }
 
-let prev=document.createElement("button")
-let flag=0
-function previous(){
-    // event.preventDefault();
-    container.innerHTML=null
-    // console.log("hi");
-   for(let i=flag; i<flag+4; i++){
-   let html=` <div id="images">
+  for (let i = flag + 4; i > flag; i--) {
+    let html = ` <div id="images">
             <img id="image" src="${arr[i].img}">
+            <div class="div1">
             <h2>${arr[i].Brand}</h2>
             <h3>${arr[i].name}</h3>
             <span>${arr[i].price}</span>
-
-            </div>`
-            container.innerHTML+=html
-            console.log(html)
-   }
-   flag--
+           </div>
+            </div>`;
+    container.innerHTML += html;
+    console.log(html);
+  }
+  flag++;
 }
-prebtn.addEventListener("click",previous)
+prebtn.addEventListener("click", previous);
 
+function next() {
+  container.innerHTML = "";
+  if (flag == 4) {
+    flag = 1;
+  }
 
-function next(){
-  if(container.innerHTML==flag+3){
-    return arr[0]
-}
-// e.preventDefault();
-container.innerHTML=null
-    // console.log("hi");
-   for(let i=flag; i<flag+4; i++){
-   let html=` <div id="images">
+  for (let i = flag; i < flag + 4; i++) {
+    let html = ` <div id="images">
             <img id="image" src="${arr[i].img}">
             <h2>${arr[i].Brand}</h2>
             <span>${arr[i].name}</span>
             <p>${arr[i].price}</p>
-            </div>`
-            container.innerHTML+=html
-            console.log(html)
-   }
-   flag++
-   
+            </div>`;
+    container.innerHTML += html;
+    console.log(html);
+  }
+  flag++;
 }
 
-nextbtn.addEventListener("click",next)
+nextbtn.addEventListener("click", next);
 
-next()
-previous()
-
+next();
+previous();
 
 
